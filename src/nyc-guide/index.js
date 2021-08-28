7ce2ac82c0142fc88992e3971bac4a0a9199da8a
Ryan Rana <39924576+RyanRana@users.noreply.github.com>
2021-08-28 10:07:47 -0400

    Add files via upload

---
+++ src/nyc-guide/index.js
@@ -0,0 +1,312 @@
+var Alexa = require('alexa-sdk');
+var http = require('http');
+
+var states = {
+    SEARCHMODE: '_SEARCHMODE',
+    TOPFIVE: '_TOPFIVE',
+};
+
+var location = "New York City";
+
+var numberOfResults = 3;
+
+var APIKey = "4844d21f760b47359945751b9f875877";
+
+var welcomeMessage = location + " Guide. You can ask me for an attraction or say help. What will it be?";
+
+var welcomeRepromt = "You can ask me for an attraction or say help. What will it be?";
+
+var locationOverview = "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.";
+
+var HelpMessage = "Here are some things you can say: Give me an attraction. Tell me about " + location + ". Tell me the top five things to do.  What would you like to do?";
+
+var moreInformation = "See your  Alexa app for  more  information."
+
+var tryAgainMessage = "please try again."
+
+var noAttractionErrorMessage = "What attraction was that? " + tryAgainMessage;
+
+var topFiveMoreInfo = " You can tell me a number for more information. For example, open number one.";
+
+var getMoreInfoRepromtMessage = "What number attraction would you like to hear about?";
+
+var getMoreInfoMessage = "OK, " + getMoreInfoRepromtMessage;
+
+var goodbyeMessage = "OK, have a nice time in " + location + ".";
+
+var hearMoreMessage = "Would you like to hear about another top thing that you can do in " + location +"?";
+
+var newline = "\n";
+
+var output = "";
+
+var alexa;
+
+var attractions = [
+    { name: "Statue of Liberty National Monument", content: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor within New York City, in the United States."},
+    { name: "Brooklyn Bridge", content: "The Brooklyn Bridge is a hybrid cable-stayed/suspension bridge in New York City, spanning the East River between Manhattan Island and Brooklyn on Long Island. Opened on May 24, 1883, the Brooklyn Bridge was the first fixed crossing of the East River. It was also the longest suspension bridge in the world at the time of its opening, with a main span of 1,595.5 feet and a deck 127 ft above mean high water."},
+    { name: "Times Square", content: "Times Square is a major commercial intersection, tourist destination, entertainment center, and neighborhood in the Midtown Manhattan section of New York City, at the junction of Broadway and Seventh Avenue. Brightly lit by numerous billboards and advertisements, it stretches from West 42nd to West 47th Streets. One of the world's busiest pedestrian areas, it is also the hub of the Broadway Theater District and a major center of the world's entertainment industry. "},
+    { name: "Empire State Building", content: "The Empire State Building is a 102-story Art Deco skyscraper in Midtown Manhattan in New York City, United States. It was designed by Shreve, Lamb & Harmon and built from 1930 to 1931. Its name is derived from Empire State, the nickname of the state of New York. "},
+    { name: "Central Park", content: "Central Park is an urban park in New York City located between the Upper West and Upper East Sides of Manhattan. It is the fifth-largest park in the city by area, covering 843 acres. It is the most visited urban park in the United States, with an estimated 42 million visitors annually as of 2016, and is the most filmed location in the world. "}
+];
+
+var topFive = [
+    {  number: "1", caption: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor within New York City, in the United States", more: "The copper statue, a gift from the people of France to the people of the United States, was designed by French sculptor Frédéric Auguste Bartholdi and its metal framework was built by Gustave Eiffel. "},
+    {  number: "2", caption:  "The Brooklyn Bridge is a hybrid cable-stayed/suspension bridge in New York City, spanning the East River between Manhattan Island and Brooklyn on Long Island", more: " Opened on May 24, 1883, the Brooklyn Bridge was the first fixed crossing of the East River. It was also the longest suspension bridge in the world at the time of its opening, with a main span of 1,595.5 feet and a deck 127 ft above mean high water."},
+    {  number: "3", caption:  "Times Square is a major commercial intersection, tourist destination, entertainment center, and neighborhood in the Midtown Manhattan section of New York City, at the junction of Broadway and Seventh Avenue", more: " Brightly lit by numerous billboards and advertisements, it stretches from West 42nd to West 47th Streets. One of the world's busiest pedestrian areas, it is also the hub of the Broadway Theater District and a major center of the world's entertainment industry. "},
+    {  number: "4", caption: "The Empire State Building is a 102-story Art Deco skyscraper in Midtown Manhattan in New York City, United States. It was designed by Shreve, Lamb & Harmon and built from 1930 to 1931", more: "Its name is derived from Empire State, the nickname of the state of New York. "},
+    {  number: "5", caption: "Central Park is an urban park in New York City located between the Upper West and Upper East Sides of Manhattan. It is the fifth-largest park in the city by area, covering 843 acres", more: " It is the most visited urban park in the United States, with an estimated 42 million visitors annually as of 2016, and is the most filmed location in the world. "}
+];
+
+
+var newSessionHandlers = {
+    'LaunchRequest': function () {
+        this.handler.state = states.SEARCHMODE;
+        output = welcomeMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
+    'getOverview': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getOverview');
+    },
+//    'getNewsIntent': function () {
+//        this.handler.state = states.SEARCHMODE;
+//        this.emitWithState('getNewsIntent');
+//    },
+    'getAttractionIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getAttractionIntent');
+    },
+    'getTopFiveIntent': function(){
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getTopFiveIntent');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
+};
+
+var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
+    'getOverview': function () {
+        output = locationOverview;
+        this.emit(':tellWithCard', output, output, locationOverview);
+    },
+    'getAttractionIntent': function () {
+        var cardTitle = location;
+        var cardContent = "";
+
+        var attraction = attractions[Math.floor(Math.random() * attractions.length)];
+        if (attraction) {
+            output = attraction.name + " " + attraction.content + newline + moreInformation;
+            cardTitle = attraction.name;
+            cardContent = attraction.content + newline + attraction.contact;
+
+            this.emit(':tellWithCard', output, cardTitle, cardContent);
+        } else {
+            this.emit(':ask', noAttractionErrorMessage, tryAgainMessage);
+        }
+    },
+    'getTopFiveIntent': function () {
+        output = topFiveIntro;
+        var cardTitle = "Top Five Things To See in " + location;
+
+        for (var counter = topFive.length - 1; counter >= 0; counter--) {
+            output += " Number " + topFive[counter].number + ": " + topFive[counter].caption + newline;
+        }
+        output += topFiveMoreInfo;
+        this.handler.state = states.TOPFIVE;
+        this.emit(':askWithCard', output, topFiveMoreInfo, cardTitle, output);
+    },
+    'AMAZON.YesIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.NoIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', HelpMessage, HelpMessage);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.HelpIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+//    'getNewsIntent': function () {
+//        httpGet(location, function (response) {
+//
+//            // Parse the response into a JSON object ready to be formatted.
+//            var responseData = JSON.parse(response);
+//            var cardContent = "Data provided by New York Times\n\n";
+//
+//            // Check if we have correct data, If not create an error speech out to try again.
+//            if (responseData == null) {
+//                output = "There was a problem with getting data please try again";
+//            }
+//            else {
+//                output = newsIntroMessage;
+//
+//                // If we have data.
+//                for (var i = 0; i < responseData.response.docs.length; i++) {
+//
+//                    if (i < numberOfResults) {
+//                        // Get the name and description JSON structure.
+//                        var headline = responseData.response.docs[i].headline.main;
+//                        var index = i + 1;
+//
+//                        output += " Headline " + index + ": " + headline + ";";
+//
+//                        cardContent += " Headline " + index + ".\n";
+//                        cardContent += headline + ".\n\n";
+//                    }
+//                }
+//
+//                output += " See your Alexa app for more information.";
+//            }
+//
+//            var cardTitle = location + " News";
+//
+//            alexa.emit(':tellWithCard', output, cardTitle, cardContent);
+//        });
+//    },
+
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    }
+});
+
+var topFiveHandlers = Alexa.CreateStateHandler(states.TOPFIVE, {
+    'getAttractionIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getAttractionIntent');
+    },
+    'getOverview': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getOverview');
+    },
+    'getTopFiveIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getTopFiveIntent');
+    },
+    'AMAZON.HelpIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+
+    'getMoreInfoIntent': function () {
+        var slotValue = 0;
+        if(this.event.request.intent.slots.attraction ) {
+            if (this.event.request.intent.slots.attraction.value) {
+                slotValue = this.event.request.intent.slots.attraction.value;
+
+            }
+        }
+
+        if (slotValue > 0 && slotValue <= topFive.length) {
+
+            var index = parseInt(slotValue) - 1;
+            var selectedAttraction = topFive[index];
+
+            output = selectedAttraction.caption + ". " + selectedAttraction.more + ". " + hearMoreMessage;
+            var cardTitle = selectedAttraction.name;
+            var cardContent = selectedAttraction.caption + newline + newline + selectedAttraction.more + newline + newline + selectedAttraction.location + newline + newline + selectedAttraction.contact;
+
+            this.emit(':askWithCard', output, hearMoreMessage, cardTitle, cardContent);
+        } else {
+            this.emit(':ask', noAttractionErrorMessage);
+        }
+    },
+
+    'AMAZON.YesIntent': function () {
+        output = getMoreInfoMessage;
+        alexa.emit(':ask', output, getMoreInfoRepromtMessage);
+    },
+    'AMAZON.NoIntent': function () {
+        output = goodbyeMessage;
+        alexa.emit(':tell', output);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+    },
+
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    }
+});
+
+exports.handler = function (event, context, callback) {
+    alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(newSessionHandlers, startSearchHandlers, topFiveHandlers);
+    alexa.execute();
+};
+
+// Create a web request and handle the response.
+function httpGet(query, callback) {
+    console.log("/n QUERY: "+query);
+
+    var options = {
+        //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=seattle&sort=newest&api-key=
+        host: 'api.nytimes.com',
+        path: '/svc/search/v2/articlesearch.json?q=' + query + '&sort=newest&api-key=' + APIKey,
+        method: 'GET'
+    };
+
+    var req = http.request(options, (res) => {
+
+            var body = '';
+
+    res.on('data', (d) => {
+        body += d;
+});
+
+    res.on('end', function () {
+        callback(body);
+    });
+
+});
+    req.end();
+
+    req.on('error', (e) => {
+        console.error(e);
+});
+}
+
+String.prototype.trunc =
+    function (n) {
+        return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
+    };
